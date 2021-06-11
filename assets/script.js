66b708665aca5f186cba802162ebc1292435cba9
grudgecat <grudgecat@protonmail.com>
2021-06-10 23:54:46 -0700

    added function to write city list of prev cities searched. Sig update to 5 day forcast fxn.

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,32 @@
-var forecastTodayEl = document.getElementById('selectedCity');
-var btnSearchEl = document.getElementById('btnSearch');
-var prevCityListEl = document.getElementById('prevCityList');
+var forecastTodayEl = $('#selectedCity');
+var btnSearchEl = $('#btnSearch');
+var prevCityListEl = $('#prevCityList');
  
-var cityName = "London";
+// var cityName = "London";
+var cityList = JSON.parse(localStorage.getItem("cities")) || [];
 var apiKey = "81d886f9c96b4f8fd57e877f07512c85";
 var iconcode = "fff";
 var iconurl = "";
-var today = moment().format("M/D/YY");
+var today = moment().format("M/D/YYYY");
 
 var lon_lat = {
     name: "",
     longitude: 0,
     latitude: 0 
 };
+
+
+
+function getCityName() {
+    var cityName = $("#txtSearch").val();
+    cityList.push(cityName);
+    if(cityList.length > 5)
+        cityList.slice(1,5);
+    localStorage.setItem("cities", JSON.stringify(cityList));
+    getCoords(cityName);
+}
+
+btnSearchEl.on('click', getCityName); 
 
 function getCoords(cityName) {
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
@@ -24,12 +38,15 @@ function getCoords(cityName) {
             lon_lat.name = data.name;
             lon_lat.longitude = data.coord.lon;
             lon_lat.latitude = data.coord.lat;
+
+            getWeatherTodayData();
+            getFiveDayForecastData();
         });
 }
 
-getCoords(cityName);
+// getCoords(cityName);
 
-function getWeatherTodayData(cityName) {
+function getWeatherTodayData() {
     //dependant on getCoords running first to set longitude/latitude
     var requestUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lon_lat.latitude}&lon=${lon_lat.longitude}&exclude=minutely,hourly,daily&appid=${apiKey}`;
     fetch(requestUrl)
@@ -40,24 +57,16 @@ function getWeatherTodayData(cityName) {
             iconurl = `"http://openweathermap.org/img/w/${data.current.weather[0].icon}.png"`
             var tempF = (((data.current.temp - 273.15) * 1.8) +32).toFixed(1);
     
-            $('#selectedCity').append(`
-            <h3>${lon_lat.name}  (${today}) </h3><img style="width:30px;height:30px" src=${iconurl} alt="Weather Icon">
+            $('#selectedCity').html(`
+            <h3>${lon_lat.name}  (${today}) </h3><img style="width:50px;height:50px" src=${iconurl} alt="Weather Icon">
             <p>Temp: ${tempF}&#8457</p>
             <p>Wind: ${data.current.wind_speed}</p>
             <p>Humidity: ${data.current.humidity}</p>
             <p>UV Index: ${data.current.uvi}</p>
             `);
-    });
+           });
 }
      
-setTimeout(getWeatherTodayData, 1000);
-
-// getWeatherTodayData(cityName);  //this works, but building URL via EVENT LISTENER the cityName in URL fails???
-//btnSearchEl.addEventListener('click', getTodayWeather); //Generates error in URL, cityName displays as mouse event???
-//when defined with 'onclick' at button, fxn runs but city is undefined in URL???
-//btnSearchEl.click(getTodayWeather);  //also fails with undefined.
-
-
 function getFiveDayForecastData(cityName) {
     var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lon_lat.latitude}&lon=${lon_lat.longitude}&units=imperial&appid=${apiKey}`
 
@@ -69,25 +78,29 @@ function getFiveDayForecastData(cityName) {
             console.log("5 day data: ")
             console.log(data);
             var i = 0;
-            for(i = 0; i < data.list.length; i+=8) {
-                console.log(data.list[i].main.temp);
-                console.log(data.list[i].wind.speed);
-                console.log(data.list[i].main.humidity);
+            $("#dailyForecasts").empty();
+            for(i = 2; i < data.list.length; i+=8) {
+                $("#dailyForecasts").append(`
+                <div class="card w-20">
+                <div class="card-body">
+                <h5 class="card-title">${moment(data.list[i].dt,"X").format("MM/DD/YYYY")}</h5>
+            	<img src="http://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png">
+                <p class="card-text">Temp: ${data.list[i].main.temp}&#8457</p>
+                <p class="card-text">Wind: ${data.list[i].wind.speed}</p>
+                <p class="card-text">Humidity: ${data.list[i].main.humidity}</p>
+              </div>
+            </div>
+                `)  
             } 
-
+            $("#txtSearch").val("");
         });
 }
 
-setTimeout(getFiveDayForecastData, 1000);
-
 function populateCitiesList() {
     //pull previously searched cities from localStorage
-    var cityList = JSON.parse(localStorage.getItem("cities")) || [];
     for(i = 0; i < cityList.length; i++) {
-        var tempListItem = document.createElement('LI');
-        tempListItem.textContent = cities[i].city;
-        prevCityListEl.appendChild(tempListItem);
+        $( "#prevCityList" ).append( `<li>${cityList[i]}</li>` );
     } 
 }
 
-
+populateCitiesList();
