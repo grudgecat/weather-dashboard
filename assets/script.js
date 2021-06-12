9466fb7a2b1562d19c4c5b5321d6d68397ffa692
grudgecat <grudgecat@protonmail.com>
2021-06-11 22:07:17 -0700

    added function to color code UVI, and to make previous city list searchable

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-var forecastTodayEl = $('#selectedCity');
 var btnSearchEl = $('#btnSearch');
 var prevCityListEl = $('#prevCityList');
  
 // var cityName = "London";
 var cityList = JSON.parse(localStorage.getItem("cities")) || [];
 var apiKey = "81d886f9c96b4f8fd57e877f07512c85";
+var numOfCities = 5;
 var iconcode = "fff";
 var iconurl = "";
 var today = moment().format("M/D/YYYY");
@@ -15,8 +15,6 @@ var lon_lat = {
     latitude: 0 
 };
 
-
-
 function getCityName() {
     var cityName = $("#txtSearch").val();
     cityList.push(cityName);
@@ -25,22 +23,39 @@ function getCityName() {
 }
 
 function checkList() {
-    console.log(cityList.length);
-    if(cityList.length > 5) {
-        // cityList.slice(1,5);
-        $(cityList).slice(1,5);
+    if(cityList.length > numOfCities) {
+        cityList.splice(0,1);     
     }
-    console.log(cityList);
     localStorage.setItem("cities", JSON.stringify(cityList));
+    populateCitiesList(); 
 }
 
 btnSearchEl.on('click', getCityName); 
+
+$('#txtSearch').keyup(function(event) {
+    if (event.which === 13) {
+        getCityName();
+    }
+});
+
+$("#prevCityList").on("click", "li", function (event) {
+    cityName = this.textContent;
+    getCoords(cityName);
+});
 
 function getCoords(cityName) {
     var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
     fetch(requestUrl)
         .then(function (response) {
-            return response.json();
+            if(response.ok)
+                return response.json();
+            else {
+                cityList.pop();
+                checkList();
+                $("#txtSearch").val("");
+                alert("Unable to find city in database, please try again."); 
+                return ""; 
+            }
         })
         .then(function (data) {
             lon_lat.name = data.name;
@@ -52,7 +67,12 @@ function getCoords(cityName) {
         });
 }
 
-// getCoords(cityName);
+function init() {
+    $("#selectedCity").html(`Enter the city you want to display in the text box on the left, 
+    then click 'Search' button or Enter key.`)
+    populateCitiesList();
+}
+
 
 function getWeatherTodayData() {
     //dependant on getCoords running first to set longitude/latitude
@@ -65,17 +85,30 @@ function getWeatherTodayData() {
             iconurl = `"http://openweathermap.org/img/w/${data.current.weather[0].icon}.png"`
             var tempF = (((data.current.temp - 273.15) * 1.8) +32).toFixed(1);
             uviNumber = data.current.uvi;
+            console.log(data);
     
             $('#selectedCity').html(`
             <h3>${lon_lat.name}  (${today}) </h3><img style="width:50px;height:50px" src=${iconurl} alt="Weather Icon">
             <p>Temp: ${tempF}&#8457</p>
             <p>Wind: ${data.current.wind_speed}</p>
             <p>Humidity: ${data.current.humidity}</p>
-            <p>UV Index: ${data.current.uvi}</p>
+            <p id="uvi">UV Index: ${data.current.uvi}</p>
             `);
-           });
+        setUVI();
+        });
 }
-     
+
+function setUVI() {
+    if(uviNumber < 3)
+        $("#uvi").css("background-color", "green");
+    else if(uviNumber < 6)
+        $("#uvi").css("background-color", "yellow");
+    else if(uviNumber < 8)
+        $("#uvi").css("background-color", "orange");
+    else
+        $("#uvi").css("background-color", "red");
+}
+
 function getFiveDayForecastData(cityName) {
     var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lon_lat.latitude}&lon=${lon_lat.longitude}&units=imperial&appid=${apiKey}`
 
@@ -84,8 +117,6 @@ function getFiveDayForecastData(cityName) {
             return response.json();
         })
         .then(function (data) {
-            // console.log("5 day data: ") 
-            // console.log(data); 
             var i = 0;
             $("#dailyForecasts").empty();
             for(i = 2; i < data.list.length; i+=8) {
@@ -110,8 +141,7 @@ function populateCitiesList() {
     //pull previously searched cities from localStorage
     for(i = 0; i < cityList.length; i++) {
         cityList = JSON.parse(localStorage.getItem("cities"));
-        $( "#prevCityList" ).append( `<li>${cityList[i]}</li>` );
+        $( "#prevCityList" ).append( `<li>${cityList[i].toUpperCase()}</li>` );
     } 
 }
 
-// populateCitiesList(); 
